a4b934353c19be9039a19ac63596ec1c98082175
Muhamad Zaenal Muttaqin <muhamad.muttaqin@klb.co.id>
2021-08-09 14:26:55 +0700

    Update Test Unit

---
+++ src/Test/ModelTestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Incubator Test.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Klb\Core\Test;
+
+use Klb\Core\Test\PHPUnit\ModelTestCase as ModelTest;
+
+/**
+ * Class serves as a placeholder for backwards compatibility
+ */
+abstract class ModelTestCase extends ModelTest
+{
+}
